32405031172c0e51a84a2ec894c117febf6eeebe
Sutherland, Landon Dexter <sutherlandon@gatech.edu>
2019-12-02 00:13:10 -0700

    Added QWIXX App to the project list

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import Content from './layouts/content';
 import logo from '../images/Gg/Gg-1.0.svg';
 import tsweeperImg from '../images/tsweeper.jpg';
 import theaterCMSImg from '../images/theater-cms.jpg';
+import qwixxImg from '../images/qwixx-app.jpg';
 
 export default () => (
   <Fragment>
@@ -13,6 +14,42 @@ export default () => (
     <Content>
       <h1>Projects</h1>
       <div className="pure-g">
+        <div className="project-container">
+          <div className="pure-u-1 pure-u-lg-1-2 project-description">
+            <div className="gutter">
+              <h2 className="project-title">QWIXX App</h2>
+              QWIXX is a table top game played with six dice and score cards.  I enjoyed the game so much
+              that I built this web app as a passion project. The main goal of this project was to make the
+              game more accessible because it is not always practicle to roll dice, for example, in the car
+              or on an airplane.  To support that goal, this app scales to any size screen gracefully and
+              is a Progressive Web App so you can install it on your device to have offline access to it
+              everywhere. You'll find a link to the rules in the app.<br/>
+              <br/>
+              One design decision that I made that is important to note is that the app does not enforce
+              any of the game rules. Since QWIXX is a table top game, families may have house rules or decide to 
+              disregard certian rules all together, and I didn't to eliminate that aspect of the game.  
+              There is a link to the game rules included in the app so users can go read up on how to play.<br/>
+              <br/>
+              Check it out at <a
+                href="https://sutherlandon.com/qwixx-app" target="_">
+                  sutherlandon.com/qwixx-app
+              </a>.
+            </div>
+          </div>
+          <div className="pure-u-1 pure-u-lg-1-2 project-sample">
+            <div className="gutter">
+              <a href="https://sutherlandon.com/qwixx-app" target="_">
+                <img className="pure-img" src={qwixxImg} alt='QWIXX App preview'/>
+              </a>
+            </div>
+          </div>
+          <div className="pure-u-1 action-container">
+            <a className="action-button" target="_"
+              href="https://github.com/sutherlandon/qwixx-app">
+              View Source
+            </a>
+          </div>
+        </div>
         <div className="project-container">
           <div className="pure-u-1 pure-u-lg-1-2 project-description">
             <div className="gutter">
