4c128f75839e6f00d78bd9f7e63b19ff4dd8c54f
Sutherland, Landon Dexter <sutherlandon@gatech.edu>
2021-02-27 00:57:18 -0700

    Added a minimalist menu and remade the FAIcon component, which is awesome

--- src/components/icon_library.js
+++ src/components/icon_library.js
@@ -1,7 +1,41 @@
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { 
+  faEdit,
+  faQuoteRight,
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faQuoteRight)
+library.add(
+  fab,
+  faEdit,
+  faQuoteRight,
+);
 
-export default FontAwesomeIcon;
+const useStyles = makeStyles((theme) => ({
+  left: {
+    paddingRight: theme.spacing(),
+  },
+  right: {
+    paddingLeft: theme.spacing(),
+  },
+}));
+
+function FAIcon(props) {
+  const { left, right, ...rest } = props;
+  const classes = useStyles();
+
+  return (
+    <FontAwesomeIcon
+      className={clsx(
+        left && classes.left,
+        right && classes.right,
+      )}
+      {...rest}
+    />
+  );
+}
+
+export default FAIcon;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,49 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
+import FAIcon from '../components/icon_library';
 import Header from '../components/Header';
 
-const useStyles = makeStyles({
-
-});
+const useStyles = makeStyles((theme) => ({
+  pageWrapper: {
+    margin: 'auto',
+    marginBottom: 38,
+    maxWidth: 1000,
+    position: 'relative',
+    top: -65,
+    width: '100%',
+  },
+  menu: {
+    margin: 'auto',
+    maxWidth: 600,
+    padding: theme.spacing(2),
+  },
+  grow: {
+    flexGrow: 1,
+  },
+}));
 
 function Home(props) {
+  const classes = useStyles();
+
   return (
-    <Header />
+    <>
+      <Header />
+      <div className={classes.pageWrapper}>
+        <Grid container className={classes.menu}>
+          <Grid item>
+            <FAIcon icon='edit' left />
+            Micro Blog
+          </Grid>
+          <Grid item className={classes.grow} />
+          <Grid item>
+            <FAIcon icon={['fab', 'github']} left />
+            Sutherlandon
+          </Grid>
+        </Grid>
+      </div>
+    </>
   );
 }
 
