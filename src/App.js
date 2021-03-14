4c2fd50953285bbe990990faddedbc4daddecffd
Sutherland, Landon Dexter <sutherlandon@gatech.edu>
2021-03-13 22:16:04 -0700

    Shortend menu items and Added the app center to the bottom of the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Redirect, Router, Route } from 'react-router-dom';
 import history from './api/history';
 
 // import './App.scss';
@@ -9,14 +9,14 @@ import { Blog, Home, Gg } from './pages';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter history={history} >
+      <Router history={history} >
         <Switch>
           <Route path='/app/home' component={Home} />
           <Route path='/app/blog' component={Blog} />
           <Route path='/app/gg' component={Gg} />
           <Redirect to='/app/home' />
         </Switch>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 
 import FAIcon from '../components/icon_library';
 import Header from '../components/Header';
 
 const useStyles = makeStyles((theme) => ({
+  appCenterWrapper: {
+    padding: theme.spacing(2),
+  },
+  appCenter: {
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    padding: theme.spacing(2),
+  },
+  appIconContainer: {
+    backgroundColor: 'gray',
+  },
+  content: {
+    flexGrow: 1,
+  },
+  column: {
+    height: '100%',
+  },
   pageWrapper: {
+    height: 'calc(100vh - 201px)',
     margin: 'auto',
-    marginBottom: 38,
     maxWidth: 1000,
     position: 'relative',
     top: -65,
@@ -31,15 +48,39 @@ function Home(props) {
     <>
       <Header />
       <div className={classes.pageWrapper}>
-        <Grid container className={classes.menu}>
+        <Grid container direction='column' className={classes.column}>
           <Grid item>
-            <FAIcon icon='edit' left />
-            Micro Blog
+            <Grid container className={classes.menu}>
+              <Grid item>
+                <FAIcon icon='edit' left />
+                Blog
+              </Grid>
+              <Grid item className={classes.grow} />
+              <Grid item>
+                <FAIcon icon={['fab', 'github']} left />
+                Github
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item className={classes.grow} />
-          <Grid item>
-            <FAIcon icon={['fab', 'github']} left />
-            Sutherlandon
+          <Grid item className={classes.content}>
+          </Grid>
+          <Grid item className={classes.appCenterWrapper}>
+            <Paper className={classes.appCenter}>
+              <Grid container spacing={2} justify='space-around'>
+                <Grid item className={classes.appIconContainer}>
+                  A
+                </Grid>
+                <Grid item className={classes.appIconContainer}>
+                  B
+                </Grid>
+                <Grid item className={classes.appIconContainer}>
+                  C
+                </Grid>
+                <Grid item className={classes.appIconContainer}>
+                  D
+                </Grid>
+              </Grid>
+            </Paper>
           </Grid>
         </Grid>
       </div>
