279bd547fab019e57e55800b1c2d6c7511b15a7e
Landon Sutherland <sutherlandon@gmail.com>
2025-07-25 15:04:19 -0600

    Added a engines flag to update to node 22

--- _src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Blog } from './blog';
-export { default as Home } from './Home';
-export { default as Gg } from './gg';
--- _src/pages/layouts/content.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default (props) => (
-  <div className="content-border">
-    <div className="content">
-      {props.children}
-    </div>
-  </div>
-);
