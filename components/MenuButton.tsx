58b0d3066bd8b10229a2eb02ebb2f0c8745a3fdc
Landon Sutherland <sutherlandon@gmail.com>
2025-12-25 08:41:19 -0700

    Fixed the menu button on a small screen on home and made the cards bigger on the smallest screen

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -2,24 +2,27 @@
 
 import { useState } from 'react';
 import clsx from 'clsx';
-import Link from "next/link";
 import FAIcon from "./FAIcon";
 import MenuContent from './MenuContent';
 import Button from './Button';
+import { usePathname } from 'next/navigation';
 
 export default function MenuButton() {
   // const backgroundColor = '#FAEEED';
   const [open, setOpen] = useState(false);
+  const pathname = usePathname();
 
   return (
     <>
       <div className='p-4 h-[87px] mt-[73px] bg-[--bg-hex]'>
-        <Button
-          className='w-fit ml-auto shadow'
-          onClick={() => setOpen(true)}
-        >
-          <FAIcon icon='bars' className='h-6 text-gray-800' />
-        </Button>
+        {pathname !== '/' && (
+          <Button
+            className='w-fit ml-auto shadow'
+            onClick={() => setOpen(true)}
+          >
+            <FAIcon icon='bars' className='h-6 text-gray-800' />
+          </Button>
+        )}
       </div>
       <div
         className={clsx(
